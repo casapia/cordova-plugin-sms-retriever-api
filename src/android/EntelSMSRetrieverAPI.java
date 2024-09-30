9572cc3ff31d67865ba311e21e622c3fd2524291
Casapia <angel.casapia@gmail.com>
2024-10-01 00:09:06 +0200

    Refactor EntelSMSRetrieverAPI.java to remove commented out code

--- src/android/EntelSMSRetrieverAPI.java
+++ src/android/EntelSMSRetrieverAPI.java
@@ -1,8 +1,10 @@
 package cl.entel.cordova;
 
 import android.util.Log;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
+
 import by.chemerisuk.cordova.support.CordovaMethod;
 import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin;
 
@@ -38,22 +40,4 @@ public class EntelSMSRetrieverAPI extends ReflectiveCordovaPlugin {
         Log.d(TAG, "stopSMSListener");
         EntelSMSRetrieverCallbackHelper.clearCallbackContext();
     }
-
-//    @CordovaMethod(WORKER)
-//    public  void requestHint(CordovaArgs args, CallbackContext callbackContext) {
-//        Log.d(TAG, "requestHint");
-//        try {
-//            HintRequest hintRequest = new HintRequest.Builder()
-//                    .setPhoneNumberIdentifierSupported(true)
-//                    .build();
-//
-//            PendingIntent intent = Auth.CredentialsApi.getHintPickerIntent(
-//                    apiClient, hintRequest);
-//            startIntentSenderForResult(intent.getIntentSender(),
-//                    RESOLVE_HINT, null, 0, 0, 0);
-//        } catch (Exception ex) {
-//            Log.e(TAG, ex.toString());
-//            callbackContext.error(ex.getMessage());
-//        }
-//    }
 }
